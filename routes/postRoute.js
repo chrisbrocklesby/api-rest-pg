64e77c6af5488281481b2bac5a209eb073e76689
Chris Brocklesby <chrisbrocklesby@users.noreply.github.com>
2021-05-11 20:28:47 +1200

    Post

--- routes/postRoute.js
+++ routes/postRoute.js
@@ -3,7 +3,7 @@ const postController = require('../controllers/postController');
 
 module.exports = (route) => {
   route.post('/posts', userController.auth, postController.create);
-  route.get('/posts', postController.readAll);
+  route.get('/posts', userController.auth, postController.readAll);
   route.get('/posts/:id', userController.auth, postController.readById);
   route.patch('/posts/:id', userController.auth, postController.update);
   route.delete('/posts/:id', userController.auth, postController.delete);
